d0fbda9be2e088bc244ef1dcac3a52065ba1c2a3
jlarrondoBirt <jlarrondo@birt.eus>
2024-09-30 19:57:44 +0200

    Pregunta 7

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -24,6 +24,10 @@ const routes: Routes = [
         loadChildren: () => import('../tab4/tab4.module').then(m => m.Tab4PageModule)
       },
       {
+        path: 'tab5',
+        loadChildren: () => import('../tab5/tab5.module').then(m => m.Tab5PageModule)
+      },
+      {
         path: '',
         redirectTo: '/tabs/tab1',
         pathMatch: 'full'
@@ -34,6 +38,10 @@ const routes: Routes = [
     path: '',
     redirectTo: '/tabs/tab1',
     pathMatch: 'full'
+  },
+  {
+    path: 'tab5',
+    loadChildren: () => import('../tab5/tab5.module').then( m => m.Tab5PageModule)
   }
 ];
 
