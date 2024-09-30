6310803d0df2ad70a12c68ac1ea6803b2d11ddd4
jlarrondoBirt <jlarrondo@birt.eus>
2024-09-30 20:00:35 +0200

    Pregunta 8

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -9,23 +9,23 @@ const routes: Routes = [
     children: [
       {
         path: 'tab1',
-        loadChildren: () => import('../tab1/tab1.module').then(m => m.Tab1PageModule)
+        loadChildren: () => import('./tab1/tab1.module').then(m => m.Tab1PageModule)
       },
       {
         path: 'tab2',
-        loadChildren: () => import('../tab2/tab2.module').then(m => m.Tab2PageModule)
+        loadChildren: () => import('./tab2/tab2.module').then(m => m.Tab2PageModule)
       },
       {
         path: 'ayuda',
-        loadChildren: () => import('../tab3/tab3.module').then(m => m.Tab3PageModule)
+        loadChildren: () => import('./tab3/tab3.module').then(m => m.Tab3PageModule)
       },
       {
         path: 'tab4',
-        loadChildren: () => import('../tab4/tab4.module').then(m => m.Tab4PageModule)
+        loadChildren: () => import('./tab4/tab4.module').then(m => m.Tab4PageModule)
       },
       {
         path: 'tab5',
-        loadChildren: () => import('../tab5/tab5.module').then(m => m.Tab5PageModule)
+        loadChildren: () => import('./tab5/tab5.module').then(m => m.Tab5PageModule)
       },
       {
         path: '',
@@ -41,7 +41,7 @@ const routes: Routes = [
   },
   {
     path: 'tab5',
-    loadChildren: () => import('../tab5/tab5.module').then( m => m.Tab5PageModule)
+    loadChildren: () => import('./tab5/tab5.module').then( m => m.Tab5PageModule)
   }
 ];
 
