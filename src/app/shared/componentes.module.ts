b445b30d9166edc01ae78702e278550c08957465
jlarrondoBirt <jlarrondo@birt.eus>
2024-10-18 11:55:16 +0200

    Pregunta 2

--- src/app/shared/componentes.module.ts
+++ src/app/shared/componentes.module.ts
@@ -1,12 +1,16 @@
+import { IonicModule } from '@ionic/angular';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { HeaderTabComponent } from './header-tab/header-tab.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [HeaderTabComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    IonicModule
+  ],
+  exports: [
+    HeaderTabComponent,
   ]
 })
 export class ComponentesModule { }
